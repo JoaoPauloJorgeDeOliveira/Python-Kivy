bf7f99d50a44b3fa18e8ee9e35aae159281e0f2f
João Paulo Jorge de Oliveira <joaoolive@gmail.com>
2020-01-12 23:44:29 -0300

    First commit. Made Hello World. Put documentation.

--- Course/main.py
+++ Course/main.py
@@ -1,4 +1,21 @@
+#encoding: utf-8
 
 from kivy.app import App
+from kivy.uix.label import Label
 
-App().run()
+def build():
+    return Label(text = 'Hello World')
+
+hello_world = App()
+hello_world.build = build       # Overriding App().build method?
+hello_world.run()
+
+'''
+Ref to make run()  work: https://stackoverflow.com/questions/40697876/unable-to-get-a-window-abort
+pip install --upgrade pip wheel setuptools
+pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+pip install kivy.deps.gstreamer
+pip install kivy.deps.angle
+pip install kivy
+
+'''
